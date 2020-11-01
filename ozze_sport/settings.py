2a070a9d5be44c65eb293d9e47b67458768b1664
SpalpIN <64166547+SpalpIN@users.noreply.github.com>
2020-11-01 22:59:59 +0200

    Update settings.py

--- ozze_sport/settings.py
+++ ozze_sport/settings.py
@@ -75,28 +75,28 @@ WSGI_APPLICATION = 'ozze_sport.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'ozze_sport_db',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'root',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
-        'TIME_ZONE': 'Europe/Kiev',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': 'ozze_sport_db',
+
+#         'USER': 'postgres',
+
+#         'PASSWORD': 'root',
+
+#         'HOST': 'localhost',
+
+#         'PORT': '5432',
+
+#         'TIME_ZONE': 'Europe/Kiev',
+
+#     }
 
 }
 
